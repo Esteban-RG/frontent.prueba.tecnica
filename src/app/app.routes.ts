af565009e363382348414f8166f88c36dc6d982e
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-10 00:57:49 -0600

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Permisos } from './permisos/permisos';
+import { TiposPermiso } from './tipos-permiso/tipos-permiso';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path: 'home', component: Home },
+    {path: 'permisos', component: Permisos },
+    {path: 'tipos-permiso', component: TiposPermiso},
+    { path: '**', redirectTo: 'home' }
+];
