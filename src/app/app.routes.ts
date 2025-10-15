cb452c14084ac2c1976efeb8f96605eb848681d2
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-15 15:44:06 -0600

    Se inyecto authguard para proyteger rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 import { Permisos } from './features/permisos/permisos';
 import { TiposPermiso } from './features/tipos-permiso/tipos-permiso';
 import { Home } from './features/home/home';
 import { Login } from './features/login/login';
+import { Solicitudes } from './features/solicitudes/solicitudes';
 
 
 
@@ -10,8 +12,9 @@ import { Login } from './features/login/login';
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     {path: 'login', component: Login },
-    {path: 'home', component: Home },
-    {path: 'permisos', component: Permisos },
-    {path: 'tipos-permiso', component: TiposPermiso},
+    {path: 'home', component: Home, canActivate: [authGuard] },
+    {path: 'permisos', component: Permisos, canActivate: [authGuard]},
+    {path: 'solicitudes', component: Solicitudes, canActivate: [authGuard]},
+    {path: 'tipos-permiso', component: TiposPermiso, canActivate: [authGuard]},
     { path: '**', redirectTo: 'home' }
 ];
