1adbf7b289e7889d9449f4d660c89eae3a7db814
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-15 15:36:50 -0600

    Se agregop informacion de sesion activa a navbar

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NgClass } from '@angular/common';
+import { AuthService } from '../../../core/services/auth-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar {
+export class Navbar implements OnInit {
+  username: string | null = null;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.username = this.authService.getUsername();
+  }
+
+  onLogout(): void {
+    console.log('Cerrando sesion...');
+    this.authService.logout();
+  }
 
 }
