1fbb76aa21cefe0877cced9ee2544709f9037904
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-15 15:45:15 -0600

    Se cambio de directorio permiso_service y se añadieron mas metodos

---
+++ src/app/features/permisos/permiso-service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Permiso } from '../../shared/models/Permiso';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermisoService {
+  private apiUrl = 'https://localhost:44325/api/Permiso';
+
+  constructor(private http: HttpClient) { }
+
+  getPermisos(): Observable<Permiso[]> {
+    return this.http.get<Permiso[]>(this.apiUrl);
+  }
+
+  getPendingPermisos(): Observable<Permiso[]> {
+    return this.http.get<Permiso[]>(`${this.apiUrl}/EstatusPendiente`);
+  }
+
+  getMyPermisos(): Observable<Permiso[]> {
+    return this.http.get<Permiso[]>(`${this.apiUrl}/MisPermisos`);
+  }
+
+  addPermiso(permisoData: Partial<Permiso>): Observable<Permiso> {
+    return this.http.post<Permiso>(this.apiUrl, permisoData);
+  }
+  
+  deletePermiso(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  updatePermiso(permisoData: Partial<Permiso>): Observable<Permiso> {
+    return this.http.put<Permiso>(`${this.apiUrl}/${permisoData.id}`, permisoData);
+  }
+
+  aprovePermiso(id: number, comentariosSupervisor: string): Observable<void> {
+    return this.http.patch<void>(`${this.apiUrl}/Aprobar/${id}`, { comentariosSupervisor });
+  }
+
+  denyPermiso(id: number, comentariosSupervisor: string): Observable<void> { 
+    return this.http.patch<void>(`${this.apiUrl}/Rechazar/${id}`, {comentariosSupervisor});
+  } 
+}
