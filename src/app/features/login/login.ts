5dc31d3feeff284014d8f87c28907e75bc58ae10
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-15 15:46:38 -0600

    Se creo componente login para iniciar sesion

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../core/services/auth-service';
-import { response } from 'express';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Card } from "../../shared/components/card/card";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, Card],
   templateUrl: './login.html',
-  styleUrl: './login.css'
 })
+
 export class Login {
-  constructor(private authService: AuthService){}
-  onLogin(formValue: any){
+  formLogin: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {
+      this.formLogin = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required]
+      });
+    }
+  
+  onLogin() {
+    if (this.formLogin.invalid) {
+      return;
+    }
+    const formValue = this.formLogin.value;
+    
     this.authService.login(formValue.email, formValue.password).subscribe({
       next:(response) => {
         console.log('Login exitoso');
