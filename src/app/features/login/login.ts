68daa5a9a1633b2a293046d63bf3a2ed270e5824
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-20 22:02:30 -0600

    Se cambio la interfaz de ventana de login

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from '../../core/services/auth-service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Card } from "../../shared/components/card/card";
 
 @Component({
   selector: 'app-login',
@@ -14,10 +13,14 @@ import { Card } from "../../shared/components/card/card";
 
 export class Login {
   formLogin: FormGroup;
+  listErrors: any[] = [];
+
+
 
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private cdr: ChangeDetectorRef
   ) {
       this.formLogin = this.fb.group({
         email: ['', [Validators.required, Validators.email]],
@@ -26,17 +29,29 @@ export class Login {
     }
   
   onLogin() {
+    this.listErrors = []; 
     if (this.formLogin.invalid) {
       return;
     }
     const formValue = this.formLogin.value;
-    
+
     this.authService.login(formValue.email, formValue.password).subscribe({
-      next:(response) => {
+      next: (response) => {
         console.log('Login exitoso');
       },
       error: (err) => {
-        console.log('Error en el login',err);
+        console.log('Error en el login', err.error);
+        const errorResponse = err.error;
+        if (Array.isArray(errorResponse)) {
+          this.listErrors = errorResponse;
+        } else if (typeof errorResponse === 'string') {
+          this.listErrors = [{ message: errorResponse }];
+        } else if (errorResponse && typeof errorResponse === 'object') {
+          this.listErrors = [errorResponse];
+        } else {
+          this.listErrors = [{ message: 'An unexpected error occurred.' }];
+        }
+        this.cdr.detectChanges();
       }
     });
   }
