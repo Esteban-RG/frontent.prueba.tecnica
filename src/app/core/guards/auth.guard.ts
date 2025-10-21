c40073d852d163ed683c4acf6ca989b4216096a3
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-20 22:00:25 -0600

    Se suscribio a metodo Observable para obtener estado del usuario

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,15 +1,19 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth-service';
+import { map, take, tap } from 'rxjs/operators';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.getToken()) {
-    return true; 
-  } else {
-    router.navigate(['/login']);
-    return false;
-  }
+  return authService.isLoggedIn$.pipe(
+    take(1),
+    tap(isLoggedIn => {
+      if (!isLoggedIn) {
+        console.log('AuthGuard: Usuario no autenticado. Redirigiendo a /login');
+        router.navigate(['/login']);
+      }
+    })
+  );
 };
