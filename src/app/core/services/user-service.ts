0a2e14292575f666a086aabebffa6a4a2ba69775
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-30 13:10:11 -0600

    Se refactorizo el servicio de gestion de usuarios

--- src/app/core/services/user-service.ts
+++ src/app/core/services/user-service.ts
@@ -1,17 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Usuario } from '../../shared/models/Usuario';
+import { environment } from '../../../environments/environment';
+import { BehaviorSubject, catchError, Observable, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'https://localhost:44325/api/User'
+  private apiUrl = `${environment.apiUrl}/api/User`;
 
   constructor(private http: HttpClient) { }
 
-  getUsers() {
-    return this.http.get<Usuario[]>(this.apiUrl);
+  private usersSource = new BehaviorSubject<Usuario[]>([]);
+  public users$ = this.usersSource.asObservable();
+
+
+  public getUsers(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.apiUrl).pipe(
+      tap(usuarios => {
+        this.usersSource.next(usuarios);
+        console.log('Obtención de usuarios completada y estado actualizado.');
+      }),
+      catchError(error => {
+        console.error('Error al obtener los usuarios:', error);
+        return throwError(() => error);
+      })
+    );
   }
 
   getUserById(id: number) {
