6745348000cec5b6f07454d5537e2d738c9d1fcf
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-30 13:07:25 -0600

    Se agrego servicio de signalr a plantilla principal para iniciar conexion con backend

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignalrService } from './core/services/signalr-service';
+import { AuthService } from './core/services/auth-service';
+import { NotificacionService } from './core/services/notificacion-service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = "Hola mundo";
+export class App  implements OnInit, OnDestroy {
+  constructor(
+    private signalrService: SignalrService,
+    private authService: AuthService,
+    private notificacionService: NotificacionService
+  ){}
+  
+  ngOnInit(): void {
+    this.authService.isLoggedIn$.subscribe(isLoggedIn => {
+      if (isLoggedIn) {
+        this.signalrService.startConnection();
+        this.notificacionService.loadInitialNotificaciones();
+      } else {
+        this.signalrService.stopConnection();
+        this.notificacionService.limpiarNotificaciones();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.signalrService.stopConnection();
+  }
 }
